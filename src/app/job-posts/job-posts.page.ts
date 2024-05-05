793d88083331bcc438c4c5d462336ac5d3fa2722
KarolisBz <25.spell@gmail.com>
2024-05-05 15:08:12 +0100

    added more appropriate search bar, added search bar cancel btn

--- src/app/job-posts/job-posts.page.ts
+++ src/app/job-posts/job-posts.page.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // used for 2 way databinding
 import { JobHandlerService } from '../Services/job-handler.service';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonInfiniteScroll, IonItem, IonList, IonInfiniteScrollContent, IonButton, IonInput, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonInfiniteScroll, IonItem, IonList, IonInfiniteScrollContent, IonButton, IonInput, IonLabel, IonSearchbar, IonIcon } from '@ionic/angular/standalone';
 import { ActivatedRoute, NavigationExtras } from '@angular/router';
 import { RouterLinkWithHref } from '@angular/router';
 import { Router } from '@angular/router';
 import { BadgeHandlerService } from '../Services/badge-handler.service';
 import { SortingAlgorithmsService } from '../Services/sorting-algorithms.service';
 
+
 @Component({
   selector: 'app-job-posts',
   templateUrl: './job-posts.page.html',
   styleUrls: ['./job-posts.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonInput, IonButton, IonInfiniteScrollContent, IonList, IonItem, IonCardSubtitle, IonCardHeader, IonCardTitle, IonCard, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonInfiniteScroll, RouterLinkWithHref]
+  imports: [IonSearchbar, IonLabel, IonInput, IonButton, IonInfiniteScrollContent, IonList, IonItem, IonCardSubtitle, IonCardHeader, IonCardTitle, IonCard, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonInfiniteScroll, RouterLinkWithHref, IonIcon]
 })
 export class JobPostsPage implements OnInit {
   // class variables
@@ -24,7 +25,7 @@ export class JobPostsPage implements OnInit {
   searchBarEntery:string = "";
 
   // constructor
-  constructor(private activatedRoute: ActivatedRoute, private jobService:JobHandlerService, private router:Router, private badgeHandlerService:BadgeHandlerService, private sortingAlgorithms:SortingAlgorithmsService) {}
+  constructor(private activatedRoute: ActivatedRoute, private jobService: JobHandlerService, private router: Router, private badgeHandlerService: BadgeHandlerService, private sortingAlgorithms: SortingAlgorithmsService) {}
 
   ngOnInit() {
     // fetching ID
@@ -32,7 +33,7 @@ export class JobPostsPage implements OnInit {
 
     // on page initilization, subscribe to api data
     this.jobService.GetJobData().subscribe(
-      (data)=>{
+      (data) => {
         // remove 1 - 60 random objects from our fixed 100 objects to simulate the non-static behaviour of API returns
         // this is for project display only, real app would call a API not from json blog
         let removeNumber = Math.floor((Math.random() * 60));
@@ -44,14 +45,14 @@ export class JobPostsPage implements OnInit {
         this.jobData = data.results;
         this.constJobData = data.results; // is not effected by sorting
 
-         // setting number of results
-         this.badgeHandlerService.setJobNum(this.jobData.length);
+        // setting number of results
+        this.badgeHandlerService.setJobNum(this.jobData.length);
 
-         // adding commas to wage strings
-         this.jobData.forEach((job: { [x: string]: any; }) => {
+        // adding commas to wage strings
+        this.jobData.forEach((job: { [x: string]: any; }) => {
           job['stringMinWage'] = this.addCommasToNumber(job['minimumSalary']);
           job['stringMaxWage'] = this.addCommasToNumber(job['maximumSalary']);
-         });
+        });
       }
     );
   }
@@ -69,24 +70,36 @@ export class JobPostsPage implements OnInit {
 
   // adds commas to a number and returns as string
   private addCommasToNumber(val: number): string {
-   if (val != null) {
-    let tag = "";
+    if (val != null) {
+      let tag = "";
 
-    // adding per hr tag if below 80 euro as wage
-    // api does not give us payment type, so for this project we will assume
-    // the cut of rate to be as below
-    if (val < 1000 && val > 80) tag = "/day";
-    else if (val < 80) tag = "/hr";
+      // adding per hr tag if below 80 euro as wage
+      // api does not give us payment type, so for this project we will assume
+      // the cut of rate to be as below
+      if (val < 1000 && val > 80) tag = "/day";
+      else if (val < 80) tag = "/hr";
 
-    return val.toLocaleString() + tag;
-   }
-   return "not disclosed";
+      return val.toLocaleString() + tag;
+    }
+    return "not disclosed";
+  }
+
+  // clears the 2 way databinding data
+  clearInput() {
+    this.searchBarEntery = "";
+    this.searchJob();
   }
 
   // sorts all jobs that are closest to search bar input in desc order in respect to their title
   // this mini search engine is not case sensitive
   public searchJob()
   {
+    // if search is empty, return default search results
+    if (this.searchBarEntery == "" || this.searchBarEntery == " ") {
+      this.jobData = this.constJobData;
+      return;
+    }
+
     let likeness:any = [];
     let termWords = this.searchBarEntery.toLowerCase().split(" ");
 
